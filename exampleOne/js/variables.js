d6f83a84917ad03cea48305d3cfe30960a639c0d
Daroco-01 <Daroco-01@hotmail.com>
2020-09-16 17:04:08 -0500

    add exampleONe folder

---
+++ exampleOne/js/variables.js
@@ -0,0 +1,14 @@
+///Scope
+let nombre = 'Fernando'
+let edad = 35 
+estatura = 1.78
+const FechaEspecial=  "20-10-2020"
+const estado_casado = true
+//var
+//const
+
+//'Tu nombre es'+ nombre + 'y tu edades' + edad
+
+
+document.write( `tu nombre es ${nombre} y tu edad es ${edad} años y mastodonte`)
+
